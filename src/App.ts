9c153a26c6e198c784c1eb33769277ca636ed348
Filip Nowakowski <filip.nowakowski2000@outlook.com>
2020-03-06 16:15:29 +0100

    Firebase configuration

--- src/App.ts
+++ src/App.ts
@@ -1,4 +1,18 @@
+import * as firebase from 'firebase-admin';
+import * as serviceAccount from './config/firebase-adminsdk.json';
+import * as config from './config/config.json';
+
 export default class App {
+
+    constructor() {
+        // Initialize firebase-admin
+        const defaultApp = firebase.initializeApp({
+            credential: firebase.credential.cert(serviceAccount as firebase.ServiceAccount),
+            databaseURL: config.firebase.databaseURL,
+            storageBucket: config.firebase.storageBucket
+        }, serviceAccount.project_id);
+        console.info(`Firebase project '${defaultApp.name}' initialized.`);
+    }
 
     async main(): Promise<number> {
         return 0;
